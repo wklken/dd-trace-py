38d2ba3a390c9488d3787960ae9a544172369286
wklken <wklken@gmail.com>
2017-06-30 23:58:10 +0800

    add mako support

---
+++ ddtrace/contrib/mako/__init__.py
@@ -0,0 +1,9 @@
+from ..util import require_modules
+
+required_modules = ['mako']
+
+with require_modules(required_modules) as missing_modules:
+    if not missing_modules:
+        from .patch import patch
+
+        __all__ = ['patch']
